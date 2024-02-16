2af3b70299172f4d72a0c2e8d1bc51c36537723f
itsnoctural <nocturalthe@gmail.com>
2024-02-16 16:17:29 +0200

    integrated platoboost

--- examples/platoboost.lua
+++ examples/platoboost.lua
@@ -1,32 +1,31 @@
-local Key = "q37Dnp1"
-local FileName = "key.txt"
-local Link = "https://google.com"
-
+local PlatoBoost = loadstring(game:HttpGet("https://raw.githubusercontent.com/itsnoctural/es-auth/main/libs/platoboost.lua"))()
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/itsnoctural/es-auth/main/src/source.lua"))()
+
+local FileName = "Key.txt"
 
 Library:CreateWindow({
 	title = "Example Title",
 	description = "Description",
 	serverCode = "HjKDVu2rAH",
     onStartup = function()
-        local isNeedKey = not (isfile(FileName) and readfile(FileName) == Key)  
+        local isNeedKey = not (isfile(FileName) and PlatoBoost:verify(readfile(FileName)))  
         
-        if isNeedKey then
+        if not isNeedKey then
             print("Passed.")
         end
 
         return isNeedKey
     end,
     onCheck = function(entered)
-        local isCorrect = entered == Key
+        local isCorrect = PlatoBoost:verify(entered)
 
         if isCorrect then
-            writefile(FileName, Key)
+            writefile(FileName, entered)
         end
 
         return isCorrect
     end,
     onCopy = function() 
-        setclipboard(Link)
+        setclipboard(PlatoBoost:getLink())
     end,
 })
